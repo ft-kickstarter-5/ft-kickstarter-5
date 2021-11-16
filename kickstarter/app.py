67dccb5dc015cb334535b59c4b316d6149730c77
ulloa <samuel.ulloa@gmail.com>
2021-11-16 11:43:53 -0800

    Added gunicorn. Added routes for the forty tepmlate in flask

--- kickstarter/app.py
+++ kickstarter/app.py
@@ -10,10 +10,29 @@ def create_app():
     # Listen to a "route"
     # '/' is the home page route
     @app.route("/")
+    @app.route("/index")
     def root():
         # query the db for all users
         campaigns = df
         # what I want to happen when somebody goes to the home page
         return render_template("index.html", title="Home", campaigns=campaigns)
+
+    @app.route("/landing")
+    def landing():
+        # query the db for all users
+        # what I want to happen when somebody goes to the home page
+        return render_template("landing.html", title="Landing")
+
+    @app.route("/generic")
+    def generic():
+        # query the db for all users
+        # what I want to happen when somebody goes to the home page
+        return render_template("generic.html", title="Generic")
+
+    @app.route("/elements")
+    def elements():
+        # query the db for all users
+        # what I want to happen when somebody goes to the home page
+        return render_template("elements.html", title="Home")
 
     return app
